1be76581d29a26cff28cd71eb200fb028008a8ee
Alan Gomes <alan.gomes@outlook.com>
2018-09-24 01:54:00 -0300

    TestCommit

--- verificaSQL.py
+++ verificaSQL.py
@@ -16,9 +16,4 @@ for linha_texto in range(len(texto)):
     linha = re.findall('(SELECT\s[\w\*\)\(\,\s]+\sFROM\s[\w]+)|(UPDATE\s[\w]+\sSET\s[\w\,\'\=]+)|(INSERT\sINTO\s[\d\w]+[\s\w\d\)\(\,]*\sVALUES\s\([\d\w\'\,\)]+)|(DELETE\sFROM\s[\d\w\'\=]+)' , texto[linha_texto])
     print(linha)
 
-
-
-#print 'id: %s; function: %s; method: %s; class: %s --total == %s' % \
- #  (id, function, method, class, total)
-
 #valida = re.findall('(INSERT|CREATE|ALTER|DROP|SELECT|DECLARE|INSERT\s+INTO|UPDATE\s+\S+\s+SET|DELETE\s+FROM|EXEC(UTE){0,1}|UNION(\s+ALL){0,1})\s', texto)
