dc0afaa8bb352b1ae5ede79ecc00dc4776d64f0c
Alan Gomes <alan.gomes@outlook.com>
2018-10-14 10:23:35 -0300

    VersaoFinal

--- sqlteste_insert.sql
+++ sqlteste_insert.sql
@@ -1,5 +1,2 @@
-Create table table1; alter table table1; drop table table1;
-select FROM users; DECLARE start INT unsigned DEFAULT 1;
+Insert into gtacemio.cemi_blacklist_aparelhos (NU_IMEI_ESN,DT_INCLUSAO_CEMI,CD_USUARIO_INCLUSAO,DT_ENVIO_INCLUSAO,CD_OPERADORA,NU_BO,TX_LOCAL_BO,NO_CIDADE,SG_UF,DT_BO,DT_EXCLUSAO_CEMI,CD_USUARIO_EXCLUSAO,DT_ENVIO_EXCLUSAO,NU_BO_EXCLUSAO,DT_BO_EXCLUSAO,SQ_ARQUIVO,STATUS,CD_OPERADORA_SSP) values ('351874083927155',sysdate,'60001',sysdate,'CLGSM','','','','GL',sysdate,null,'60001',null,'01',null,0,'R',null);
 INSERT INTO table_name (field1, field2) VALUES (value1, value2);
-UPDATE user SET name = "Bobby Tables"; DELETE FROM table1;EXECUTE stmt;
-(SELECT a FROM t1 WHERE a=10) UNION (SELECT a FROM t2 WHERE a=11)ORDER BY a
--- verificaSQL.py
+++ verificaSQL.py
@@ -3,19 +3,22 @@ import sys
 import string
 import re
 
-arquivo = open('./sqlteste_insert_original.sql', 'r')
+arquivo = open('./sqlteste_insert.sql', 'r')
 texto = arquivo.read()
 
-
+texto = texto.lower()
 texto = texto.replace("\r","") #Remove o \r, facilitar a tratativa de expressões regulares 
 texto = texto.replace("\n","") #Remove a quebra de linhas
 texto = texto.split(';') #Separa os comandos pelo ;
 
 
-for linha_texto in range(len(texto)):
-    linha = re.findall('(SELECT\s[\w\*\)\(\,\s]+\sFROM\s[\w]+)|(UPDATE\s[\w]+\sSET\s[\w\,\'\=]+)|(INSERT\sINTO\s[\d\w]+[\s\w\d\)\(\,]*\sVALUES\s\([\d\w\'\,\)]+)|(DELETE\sFROM\s[\d\w\'\=]+)' , texto[linha_texto])
-    print(linha)
 
-#valida = re.findall('(INSERT|CREATE|ALTER|DROP|SELECT|DECLARE|INSERT\s+INTO|UPDATE\s+\S+\s+SET|DELETE\s+FROM|EXEC(UTE){0,1}|UNION(\s+ALL){0,1})\s', texto)
+
+for linha_texto in range(len(texto)-1):
+    padraoregex = re.compile(r"(INSERT INTO\s+)([\w._]+)(\s+\()([\w+,?\s*]+)(\)\s+VALUES\s+\()(['?\w+'?,?\s*]+)(\))", re.IGNORECASE)
+    linha = re.match(padraoregex, texto[linha_texto])
+    print(f'A tabela {linha.group(2)} está sendo alterada')
+
+#valida = re.findall('( |CREATE|ALTER|DROP|SELECT|DECLARE|INSERT\s+INTO|UPDATE\s+\S+\s+SET|DELETE\s+FROM|EXEC(UTE){0,1}|UNION(\s+ALL){0,1})\s', texto)
 #Novo teste
 #Teste3
