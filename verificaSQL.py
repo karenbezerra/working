9e5f9463ec7e3f9a9b1ff0aaadcb60f322e0560f
Alan Gomes <alan.gomes@outlook.com>
2018-10-14 11:21:14 -0300

    Corrigido o bug de tratativa em multiplas linhas

--- verificaSQL.py
+++ verificaSQL.py
@@ -3,7 +3,7 @@ import sys
 import string
 import re
 
-arquivo = open('./sqlteste_insert_original.sql', 'r')
+arquivo = open('./sqlteste_insert.sql', 'r')
 texto = arquivo.read()
 
 #texto = texto.lower() #Desativei, pois agora o re.IGNORECASE foi ativado
@@ -15,10 +15,10 @@ texto = texto.split(';') #Separa os comandos pelo ;
 
 
 for linha_texto in range(len(texto)-1): #Lê o array do arquivo e remove a ultima linha após a formatação. O split causa um linha em branco, por isso o -1
-    padraoregex = re.compile(r"(INSERT INTO\s+)([\w._]+)(\s+\()([\w+,?\s*]+)(\)\s+VALUES\s+\()(['?\w+'?,?\s*]+)(\))", re.IGNORECASE) #Regex, o re.IGNORECASE trata /i que o python n~ão possui, além disso procura os padrões 'INSERT INTO' e 'VALUES', a partir disso ele cria os grupos de busca: $2 nome da tabela, $4 campos e $6 os valores, para melhor visualizar recomendo o https://www.debuggex.com/
+    padraoregex = re.compile(r"(INSERT INTO\s+)([\w._]+)(\s+\()([\w+,?\s*]+)(\)\s+VALUES\s+)((\(['?\w+'?,?\s*]+\)\,?;?\s*)+)", re.IGNORECASE) #Regex, o re.IGNORECASE trata /i que o python n~ão possui, além disso procura os padrões 'INSERT INTO' e 'VALUES', a partir disso ele cria os grupos de busca: $2 nome da tabela, $4 campos e $6 os valores, para melhor visualizar recomendo o https://www.debuggex.com/
     linha = re.match(padraoregex, texto[linha_texto]) #Executa o regex para cada linha
     if linha is not None: #None é o retorno padão para o Regex não encontrado, logo podemos trata-lo como marcador para o que não é um insert
         print(f'A tabela {linha.group(2)} está sendo alterada') #Imprime apenas o grupo $2
     else:
-        print(f"Os seu script não contém apenas inserts ou não está formatado corretamente a linha {linha_texto} não foi inserida, por gentileza validar") #Bug, se um comando está quebrado em duas linhas, ele não irá reconhecer, precisamos tratar e juntar as linhas pelo ;
+        print(f"Os seu script não contém apenas inserts ou não está formatado corretamente a linha {linha_texto+1} não foi inserida, por gentileza validar") #Resolvido o bug de multiplas linhas com o Regex
    
